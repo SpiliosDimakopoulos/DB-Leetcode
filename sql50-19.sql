ce69eeed8a004f441af34311ffb7ba180c9cce6b
Spilios Dmk <150211937+SpiliosDimakopoulos@users.noreply.github.com>
2025-01-11 16:09:03 +0200

    Delete sql50-19.sql

--- sql50-19.sql
+++
@@ -1,7 +0,0 @@
--- 1211. Queries Quality and Percentage
-
-SELECT DISTINCT query_name, 
-                ROUND(AVG(CAST(rating as decimal) / position), 2.0) AS quality,
-                ROUND(SUM(case when rating < 3 then 1 else 0 end) * 100 / count(*), 2) AS poor_query_percentage
-FROM Queries
-GROUP BY query_name;
