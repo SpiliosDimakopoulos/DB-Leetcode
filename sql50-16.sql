122f5975564ecea878c67d56e6d2fb08e63b06ef
Spilios Dmk <150211937+SpiliosDimakopoulos@users.noreply.github.com>
2025-01-11 16:08:36 +0200

    Delete sql50-16.sql

--- sql50-16.sql
+++
@@ -1,8 +0,0 @@
--- 1251. Average Selling Price
-
-SELECT p.product_id, IFNULL(ROUND(SUM(p.price*u.units)/SUM(u.units),2),0) as average_price
-FROM Prices p 
-LEFT JOIN UnitsSold u
-ON p.product_id = u.product_id AND 
-u.purchase_date BETWEEN p.start_date and p.end_date
-GROUP BY p.product_id;
