696c3f17b8034dd6214a90a564de8d0c67e3f977
Spilios Dmk <150211937+SpiliosDimakopoulos@users.noreply.github.com>
2025-01-11 16:08:24 +0200

    Delete sql50-14.sql

--- sql50-14.sql
+++
@@ -1,7 +0,0 @@
--- 1934. Confirmation Rate
-
-SELECT s.user_id, ROUND(AVG(IF(c.action = "confirmed", 1.0, 0.0)), 2) as confirmation_rate
-FROM Signups s
-LEFT JOIN Confirmations c
-ON s.user_id = c.user_id
-GROUP BY user_id;
