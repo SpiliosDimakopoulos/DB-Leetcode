64ca226f53ce62cd71b8ad57d98b5db8d6f21f2c
Spilios Dmk <150211937+SpiliosDimakopoulos@users.noreply.github.com>
2025-01-11 16:09:43 +0200

    Delete sql50-21.sql

--- sql50-21.sql
+++
@@ -1,9 +0,0 @@
--- 1174. Immediate Food Delivery II
-
-SELECT 
-    ROUND(
-        (SUM(order_date = customer_pref_delivery_date) / COUNT(*)) * 100, 2
-    ) AS immediate_percentage
-FROM Delivery
-WHERE (customer_id,order_date) IN (SELECT customer_id,min(order_date) FROM delivery 
-GROUP BY customer_id);
