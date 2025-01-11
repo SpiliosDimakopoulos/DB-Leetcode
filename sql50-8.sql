1da32b26a17be638d6f7066b686ed56c37847f4e
Spilios Dmk <150211937+SpiliosDimakopoulos@users.noreply.github.com>
2025-01-11 16:10:27 +0200

    Delete sql50-8.sql

--- sql50-8.sql
+++
@@ -1,6 +0,0 @@
--- 1581. Customer Who Visited but Did Not Make Any Transactions
-
-SELECT Visits.customer_id, COUNT(Visits.visit_id) AS count_no_trans 
-FROM Visits
-WHERE Visits.visit_id NOT IN (SELECT visit_id FROM Transactions)
-GROUP BY customer_id;
