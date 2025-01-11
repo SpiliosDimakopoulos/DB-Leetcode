eb90867a15503a05085fc718b21343250aa8ddbe
Spilios Dmk <150211937+SpiliosDimakopoulos@users.noreply.github.com>
2025-01-11 16:08:30 +0200

    Delete sql50-18.sql

--- sql50-18.sql
+++
@@ -1,7 +0,0 @@
--- 1633. Percentage of Users Attended a Contest
-
-SELECT contest_id, 
-ROUND(COUNT(DISTINCT user_id) * 100 /(SELECT count(user_id) FROM Users) ,2) AS percentage
-FROM  Register
-GROUP BY contest_id
-ORDER BY percentage DESC,contest_id;
